ad14a313e029e62f0b353ecf4676efc9bc7c1718
mirikhann <60973055+mirikhann@users.noreply.github.com>
2023-05-07 12:31:44 +0400

    Update Ebob&Ekob.php

--- Ebob&Ekob.php
+++ Ebob&Ekob.php
@@ -1,12 +1,14 @@
-<?php
-
 function divisors($val){
 	
-	$arr = [];
+	if($val<=2){
+		return [2];
+	}
 	
-	for($i=2; $i<=$val; $i++){
+	$arr = [2];
+	
+	for($i=3; $i<=$val; $i+=2){
 		$check = 0;
-		for($j=2; $j<$i; $j+=2){
+		for($j=2; $j<$i; $j++){
 			if($i%$j==0){
 				$check = 1;
 				break;
@@ -22,6 +24,10 @@ function divisors($val){
 
 function divide($val)
 {
+	if($val<=2){
+		return [2];
+	}
+	
 	$divisors = divisors($val);
 	$ind = 0;
 	$divideVal = $divisors[$ind];
@@ -86,7 +92,3 @@ function ekob ($a,$b){
 	},$large);
 	
 }
-
-
-print ekob(36,52);
-// print ebob(36,52);
